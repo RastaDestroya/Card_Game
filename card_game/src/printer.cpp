bc070daca82affaca7816b6d838d611ffed365b2
RastaDestroya <87921591+RastaDestroya@users.noreply.github.com>
2021-07-25 04:50:04 +0100

    Add files via upload

---
+++ card_game/src/printer.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+{
+
+    // std::cout<<"\n";
+    // std::cout<<".------..------."<<"\n";
+    // std::cout<<"|"<<"A"<<".--. || .--. |"<<"\n";
+    // std::cout<<"| :(): |"; std::cout<<"|  //  |"<<"\n";
+    // std::cout<<"| ()() |"; std::cout<<"|  //  |"<<"\n";
+    // std::cout<<"| '--'"<<"A"<<"|| '--' |"<<"\n";
+    // std::cout<<"`------'`------'"<<"\n";
+    
+
+
+                std::cout<<".------.\n";
+                std::cout<<"|"<<"K"<<".--. |\n"; 
+                std::cout<<"| :(): |\n";
+                std::cout<<"| :(): |\n"; 
+                std::cout<<"| '--'"<<"K"<<"|\n";
+                std::cout<<"`------'\n";
+
+
+    system("pause");
+    return 0;
+}
